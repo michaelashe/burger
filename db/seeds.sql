dd30975d94e719fa1422021f229ff8862dc9fa88
michaelashe <CherieAshe@cheries-mbp.attlocal.net>
2018-01-18 11:48:54 -0500

    Directories and Files Added

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Diablo Sandwich', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Abomination', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Heart Attack Stack', false);
+
